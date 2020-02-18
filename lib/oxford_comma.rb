ce94bb08b6e59ffadf7840a05b5da6c3c9995362
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-18 21:32:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,6 @@
 def oxford_comma(array)
-  if array.length == 1
-    return "#{array[0]}"
-  elsif array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
-    array[-1] = "and #{array[-1]}"
-    return array.join(", ")
+  if array.length == 1 
+    return
   end
 end
 
